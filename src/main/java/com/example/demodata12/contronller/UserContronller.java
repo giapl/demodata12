c9f8bee9d80b2ee47b93dfa708dddc7a0f83d5ae
giapl <ledanggiap1704@gmail.com>
2023-07-03 21:18:01 +0700

    two

--- src/main/java/com/example/demodata12/contronller/UserContronller.java
+++ src/main/java/com/example/demodata12/contronller/UserContronller.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,7 +41,7 @@ public class UserContronller {
     return ResponseEntity.ok(userRequest);
   }
   @DeleteMapping("/delete")
-  public ResponseEntity<?> deleteById(@PathVariable Long id){
+  public ResponseEntity<?> deleteById(@RequestParam Long id){
     iUserService.deleteById(id);
     return ResponseEntity.ok("dã xoa thanh cong account có id: "+ id);
   }
